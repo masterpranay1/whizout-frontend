feaf6f2524b71728a46e85ccebaf0cfb015dd036
Pranay Raj <masterpranayraj@gmail.com>
2024-02-21 01:16:18 +0530

    refactor: app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import PostsSection from "@/components/PostsSection";
+import HomepageSidebar from "@/components/HomepageSidebar";
+
+export default async function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Home</h1>
+    <div className="w-full flex flex-row px-8 py-8">
+      <HomepageSidebar />
+      <PostsSection />
     </div>
   );
 }
